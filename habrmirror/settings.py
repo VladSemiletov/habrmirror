28542f52f2d002654d5e3e525296bd0d347d8951
vivl <semiletoffvladislav@yandex.ru>
2022-07-19 12:44:23 +0300

    заменил БД sqlite3 на mysql

--- habrmirror/settings.py
+++ habrmirror/settings.py
@@ -12,6 +12,10 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import os
+import pymysql
+
+
+pymysql.install_as_MySQLdb()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -77,8 +81,12 @@ WSGI_APPLICATION = 'habrmirror.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'habrmirror',
+        'USER': 'vivl',
+        'PASSWORD': 'ViVl762#',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
