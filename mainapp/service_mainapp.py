51d7211c6d2bac92a494b3c2f10a15ee6af0c754
Fedortsov-AV <shalf@mail.ru>
2022-07-21 16:52:57 +1000

    Вставил в шаблон главной страницы добавление новых статей из БД

--- mainapp/service_mainapp.py
+++ mainapp/service_mainapp.py
@@ -1,9 +1,9 @@
 from django.db.models import QuerySet
 
-from models import Hab
+from mainapp.models import Hab
 
 
 def last_hub() -> QuerySet:
     """Функция выбирает последние 5 созданных статей"""
-    query_set = Hab.objects.oobjects.all().order_by("creat_time")[:5]
+    query_set = Hab.objects.all().order_by("creat_time")[:5]
     return query_set
