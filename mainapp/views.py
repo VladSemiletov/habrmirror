c7effdcdb4fc6a9f2bf88edecaa8333c1d9b4210
Fedortsov-AV <shalf@mail.ru>
2022-07-21 13:45:22 +1000

    Создан каркас контроллера для главной страницы

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+def index(request):
+    """Функция мениет контексn для главной страницы и рендерит ее"""
+    context = {}
+    return render(request, 'mainapp/index.html', context)
