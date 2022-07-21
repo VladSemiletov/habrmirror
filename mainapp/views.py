173a879478349590a8140643714dad3bc5b3c25a
Fedortsov-AV <shalf@mail.ru>
2022-07-21 15:51:27 +1000

    Доработан контроллер главной страницы: в контекст главной страницы передается QuerySet с 5 последними публикациями

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
 from core.context_service import context_update
 
-
 # Create your views here.
+from mainapp.service_mainapp import last_hub
+
+
 def index(request):
     """Функция мениет контексn для главной страницы и рендерит ее"""
     context = {}
     context_update(context, key='Title', value='HabrMirror')
+    context_update(context, key='hab', value=last_hub())
     return render(request, 'mainapp/index.html', context)
