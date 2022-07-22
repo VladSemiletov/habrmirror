632826ec6853a134c7582dcfaf7fdd015c734402
Fedortsov-AV <shalf@mail.ru>
2022-07-22 14:34:59 +1000

    Прописана модель авторизации в settings.py

--- habrmirror/settings.py
+++ habrmirror/settings.py
@@ -141,6 +141,9 @@ STATICFILES_DIRS = (
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+AUTH_USER_MODEL = 'userapp.User'
+LOGIN_URL = '/users/login/'
+LOGIN_REDIRECT_URL = 'index'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
