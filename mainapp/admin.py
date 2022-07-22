76b6ac7d4b77a7b01e6837512376657316310e94
Fedortsov-AV <shalf@mail.ru>
2022-07-22 15:21:15 +1000

    Прописаны таблицы Hab и HabCategory в админке

--- mainapp/admin.py
+++ mainapp/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from userapp.models import User, UserProfile
+from mainapp.models import HabCategory, Hab
 
 # Register your models here.
-admin.site.register(User)
-admin.site.register(UserProfile)
+admin.site.register(Hab)
+admin.site.register(HabCategory)
--- userapp/admin.py
+++ userapp/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from userapp.models import User, UserProfile
 
 # Register your models here.
+admin.site.register(User)
+admin.site.register(UserProfile)
