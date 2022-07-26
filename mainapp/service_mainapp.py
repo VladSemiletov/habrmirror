52f24f90197e8cbb807fb7643340928184881086
Fedortsov-AV <shalf@mail.ru>
2022-07-27 01:46:26 +1000

    Поправил выборку 5 последних постов

--- mainapp/service_mainapp.py
+++ mainapp/service_mainapp.py
@@ -5,7 +5,9 @@ from mainapp.models import Hab, HabCategory
 
 def last_hub() -> QuerySet:
     """Функция выбирает последние 5 созданных статей"""
-    query_set = Hab.objects.all().order_by("creat_time")[:5:-1]
+    query_set = Hab.objects.all().order_by("creat_time")
+    count = query_set.count()
+    query_set = query_set[count-5:count:-1]
     return query_set
 
 
