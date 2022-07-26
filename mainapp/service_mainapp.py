4f8da2759d9060347fc7bb2e9c3e225f8d21fcd3
Fedortsov-AV <shalf@mail.ru>
2022-07-27 00:42:11 +1000

    Восстановлена работоспособность проекта, вернул сеттинг и т.д. в шаблоне изменил главное меню, теперь разделы подгружаются из бд, так же добавил кнопку войти/выйти в зависемости от подключившегося пользователя

--- mainapp/service_mainapp.py
+++ mainapp/service_mainapp.py
@@ -1,9 +1,15 @@
 from django.db.models import QuerySet
 
-from mainapp.models import Hab
+from mainapp.models import Hab, HabCategory
 
 
 def last_hub() -> QuerySet:
     """Функция выбирает последние 5 созданных статей"""
     query_set = Hab.objects.all().order_by("creat_time")[:5:-1]
+    return query_set
+
+
+def all_HabCategory() -> QuerySet:
+    """Функция выбирает все категории статей"""
+    query_set = HabCategory.objects.all()
     return query_set
