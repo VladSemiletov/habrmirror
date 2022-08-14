b2e870087ace0ddf59340b6e53feff5dd70657d8
Mikhail <smyllik4@bk.com>
2022-08-14 14:16:01 +0300

    убрал таблицу mainapp_habcategory, оставил habapp_category

--- mainapp/models.py
+++ mainapp/models.py
@@ -1,16 +1,16 @@
-from django.db import models
-from django.utils import timezone
-
-from authapp.models import HabUser
-
-
-class HabCategory(models.Model):
-    """Модель описывает категории статей"""
-    name = models.CharField(max_length=64, unique=True)
-    description = models.TextField(blank=True)
-
-    class Meta:
-        verbose_name_plural = 'Hab Categories'
-
-    def __str__(self):
-        return self.name
+# from django.db import models
+# from django.utils import timezone
+#
+# from authapp.models import HabUser
+#
+#
+# class HabCategory(models.Model):
+#     """Модель описывает категории статей"""
+#     name = models.CharField(max_length=64, unique=True)
+#     description = models.TextField(blank=True)
+#
+#     class Meta:
+#         verbose_name_plural = 'Hab Categories'
+#
+#     def __str__(self):
+#         return self.name
